04d4f3983ee7a1ddc8cea62985273155118fe625
Pinar <pinaradanir96@gmail.com>
2021-06-21 06:20:13 -0500

    label text didn't show well

---
+++ app_design.py
@@ -0,0 +1,68 @@
+import pandas as pd 
+import datetime
+from tkinter import * 
+from tkcalendar import *
+
+from class_food import *
+from class_entry_food import *
+
+#app
+
+master = Tk()
+master.title(FoneFrid)
+canvas = Canvas(master, bg="#FCF0E4", height=750, width=750)
+canvas.pack()
+
+class Fonefridge(object):
+    def __init__(self, master):
+      
+        app_frame = Frame(master)
+        app_frame.pack()
+
+        #visual part(no function):
+        #AREAS:
+        #top frame
+        self.frame_top = Frame(master, bg="#A9B6BE")
+        self.frame_top.place(relx=0.01, rely=0.01, relheight=0.1, relwidth=0.98)
+
+        #bottom left frame 
+        self.frame_bottom_left = Frame(master, bg="#E9BFA7")
+        self.frame_bottom_left.place(relx=0.01, rely=0.12, relheight=0.87, relwidth=0.4)
+
+        #bottom right frame
+        self.frame_bottom_right = Frame(master, bg= "#BE796D")
+        self.frame_bottom_right.place(relx=0.42, rely=0.12, relheight=0.87, relwidth=0.57)
+
+        #TITLES:
+        #main title-FONEFRIDGE
+        self.title = Label(self.frame_top, bg="#A9B6BE", text= "FONEFRIDGE", font="roboto 22")
+        self.title.pack(pady=14)
+
+        #food name
+        self.title = Label(self.frame_bottom_left, bg="#E9BFA7", text= "Search the name of your item:", font="roboto 15")
+        self.title.pack(pady=14)
+
+
+
+
+        #text entry slot
+        self.entry_name = Entry(master, textvariable=self.name, width=50)
+        self.entry_name.pack(padx=50, pady=20)
+
+        self.name = StringVar()    #IntVar() & DoubleVar()      self.entry_name.get(command=Store_name)
+
+        self.search_button = Button(self.frame_bottom_left, text="Search", command=self.Display-item)
+        self.search_button.pack(padx=70, pady=20)
+
+    def Display_item(self):
+        print("results for", self.name, ":")
+        return Food.display_food(Food(self.name))
+
+    #def Store_name(self):
+        #name = return self.name
+
+print(name)
+
+e = Fonefridge(master)
+
+master.mainloop()
