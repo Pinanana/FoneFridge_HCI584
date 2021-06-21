a12d097e01d8fcb5a25b8632bb137194bbc7a4e8
Pinar <pinaradanir96@gmail.com>
2021-06-21 07:31:43 -0500

    put the csv file reading in the class

--- class_entry_food.py
+++ class_entry_food.py
@@ -2,27 +2,36 @@ import pandas as pd
 import datetime
 from class_food import *
 
-df = pd.read_csv("popular_items_library.csv")
+#df = pd.read_csv("popular_items_library.csv")
 #name = input("search foods from popular items library:")
 #name = "strawberry"
 
-item_info = df.query("title == @name")
+#item_info = df.query("title == @name")
 
-if len(item_info) > 0:
+#if len(item_info) > 0:
     #print("error, more than one result found.")
-    item_info.reset_index(drop=True, inplace=True)
+    #item_info.reset_index(drop=True, inplace=True)
     #print(res)
-    item_info = item_info.loc[0]
+    #item_info = item_info.loc[0]
 
 
 class Entry_Food(Food):
     def __init__(self, name, amount):
+        
+        self.df = pd.read_csv("popular_items_library.csv")
+        self.item_info = self.df.query("title == @name")
+
+        if len(self.item_info) > 0:
+            #print("error, more than one result found.")
+            self.item_info.reset_index(drop=True, inplace=True)
+            #print(res)
+            self.item_info = self.item_info.loc[0]
 
         self.name = name
-        self.typef = item_info[1]
-        self.notifyd = item_info[2]
-        self.expired = item_info[3]
-
+        self.typef = self.item_info[1]
+        self.notifyd = self.item_info[2]
+        self.expired = self.item_info[3]
+    
         self.amount = amount
 
         self.today = datetime.datetime.today().date()
@@ -40,10 +49,10 @@ class Entry_Food(Food):
 
 
 #get this value from user
-amount = 5
+#amount = 5
 
 
-food_itemm = Entry_Food(name, amount)
+#food_itemm = Entry_Food(name, amount)
 #print(Entry_Food.display_food_entry(food_itemm))
 #print(Entry_Food.expiration_date(food_itemm))
 #print(Entry_Food.notification_date(food_itemm))
