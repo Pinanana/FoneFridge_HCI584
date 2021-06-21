26d17691c64da6e052d4ebbee5cecba915c796d8
Pinar <pinaradanir96@gmail.com>
2021-06-21 06:58:26 -0500

    still trying to show result

--- copy_stuff_here_to_not_lose.py
+++ copy_stuff_here_to_not_lose.py
@@ -9,7 +9,7 @@ from class_entry_food import *
 
 
 master = Tk()
-master.title(FoneFrid)
+master.title("FoneFridge")
 canvas = Canvas(master, bg="#FCF0E4", height=750, width=750)
 canvas.pack()
 
@@ -54,10 +54,12 @@ class Fonefridge(object):
         self.search_button = Button(self.frame_bottom_left, text="Search", command=self.display_item)
         self.search_button.pack(padx=10, pady=5)
 
-        self.result = Label(self.frame_bottom_right, text="Here is the result for"+self.name.get()+":\n\n"+Food.display_food(Food(name))+"\n"+Food.display_expire(Food(name)))+"\n"+Food.display_notify(Food(name)), bd=1, relief="sunken", justify="left", bg="#BE796D", font="roboto 11")
+        self.result = Label(self.frame_bottom_right, bd=1, relief="sunken", justify="left", bg="#BE796D", font="roboto 11")
         self.result.pack(padx=10, pady=5, ipadx=5, ipady=5)
 
-   
+    def display_item(self):
+        self.result.config(text="Here is the result for"+self.name.get()+":\n\n"+Food.display_food(Food(name))+"\n"+Food.display_expire(Food(name)))+"\n"+Food.display_notify(Food(name)))
+
 
     #def Store_name(self):
         #name = return self.name
