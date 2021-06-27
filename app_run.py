d9ebd1859e0acd6e01652e48a40d7adbff64f751
Pinar <pinaradanir96@gmail.com>
2021-06-27 10:42:09 -0500

    calendar works  gets dates. notification trigger is a bit confusing. still working on it.

--- app_run.py
+++ app_run.py
@@ -16,6 +16,7 @@ canvas.pack()
 
 class Fonefridge(object):
     def __init__(self, master):
+        self.df = pd.read_csv("popular_items_library.csv")
         self.df_user = pd.read_csv("user_items.csv")
 
         app_frame = Frame(master)
@@ -27,7 +28,7 @@ class Fonefridge(object):
         self.frame_top.place(relx=0.01, rely=0.01, relheight=0.06, relwidth=0.98)
 
         #changing BOTTOM frame:
-        self.frame_change = Frame(master)
+        self.frame_change = Frame(master, bg="#E9BFA7")
         self.frame_change.place(relx=0.01, rely=0.08, relheight=0.91, relwidth=0.98)
 
 
@@ -70,7 +71,7 @@ class Fonefridge(object):
     def calendar_entry(self):
         self.pop_up = Toplevel(master)
 
-        self.date_label = Label(self.pop_up, text='Choose date')
+        self.date_label = Label(self.pop_up, text="Choose date", font="roboto 12")
         self.date_label.pack(padx=10, pady=10)
 
         self.entry_cal = DateEntry(self.pop_up, background= "#A9B6BE", foreground= "#576566")
@@ -82,12 +83,29 @@ class Fonefridge(object):
     def calendar_get(self):
         self.entry_date = self.entry_cal.get_date()
         print(self.entry_date)
+        self.notification_trigger()
         self.pop_up.destroy()
 
-    #def notification_trigger(self):
+    def notification_trigger(self):
+        self.expi = self.df_user["expiration (days)"]
+        self.noti = self.df_user["notify (days)"]
+
+
+
+        if self.noti <= self.entry_date <= self.expi:
+            self.frame_warning = Frame(master, bg="#BE796D")
+            self.frame_warning.place(relx=0.05, rely=0.05, relheight=0.95, relwidth=0.95)
+
+            self.noti_title = Label(self.frame_warning, text="WARNING", font="roboto 22")
+            self.noti_title.pack(padx=10, pady=10)
+
+            self.notification_text = Text(self.frame_warning, bg="#BE796D", font="roboto 15", height=200, width=450 bd=0)
+            self.notification_text.pack(padx=10, pady=10)
+            self.notification_text.insert(END, Entry_Food.notification_message(Entry_food(self.NAMEEEEEEEEE)))
+
+
 
 
 e = Fonefridge(master)
 
 master.mainloop()
-print()
