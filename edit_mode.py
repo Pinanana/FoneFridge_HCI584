e4beaa92d78078fa6f539653287c37ff06f5df96
Pinar <pinaradanir96@gmail.com>
2021-07-07 11:33:48 -0500

    tried to put dropdown menus inside treeview widget but apperantly that is impossible so I'll check what else I can use to display / edit / delete items

--- edit_mode.py
+++ edit_mode.py
@@ -1,4 +1,4 @@
-
+from tkinter.ttk import Style, Treeview
 from os import name
 import pandas as pd 
 import datetime
@@ -43,15 +43,75 @@ class Edit(object):
 
         #TITLES:
         #food name search title-Search the name of your item:
-        self.title = Label(self.frame_edit, bg="#576566", text= "INVENTORY", font="roboto 15")
+        self.title = Label(self.frame_edit, bg="#576566", text= "EDIT INVENTORY", font="roboto 15")
         self.title.place(relx=0.5, rely=0.01, anchor="n")
 
+        
+        self.style_tw = Style()
+        self.style_tw.theme_use("default")
+        self.style_tw.configure("Treeview", foreground="black", rowheight=25, fieldbackground="#FCF0E4")
+        self.style_tw.map("Treeview")
 
-        self.user_inventory_edit = tksheet.Sheet(self.frame_edit, data=self.df_user, height=570, width=700)
-        self.user_inventory_edit.place(relx=0.5, rely=0.1, anchor="n")
-        self.user_inventory_edit.enable_bindings(("single_select", "row_select", "column_width_resize", "arrowkeys", "right_click_popup_menu", "rc_select", "rc_insert_row", "rc_delete_row", "copy", "cut", "paste", "delete", "undo", "edit_cell"))
+        #treeview item display:
+        self.user_inventory = Treeview(self.frame_edit)
+        self.user_inventory.place(rely=0.1, relx=0.5, relwidth=0.98, relheight=0.88, anchor="n")
 
-        self.user_inventory_edit.highlight_rows(rows=[0], bg="#C2D7D0", fg=None, highlight_index=True, redraw=False)
+        self.user_inventory["column"] = list(self.df_user.columns)
+        self.user_inventory["show"] = "headings"
+
+        self.user_inventory.column("#0", width=5)
+        self.user_inventory.column("title", width=95)
+        self.user_inventory.column("type", width=95)
+        self.user_inventory.column("amount", width=95)
+        self.user_inventory.column("entry date", width=140)
+        self.user_inventory.column("notify (days)", width=140)
+        self.user_inventory.column("expiration (days)", width=140)
+
+        self.user_inventory.heading("#0", text="")
+        self.user_inventory.heading("title", text="TITLE", anchor="w")
+        self.user_inventory.heading("type", text="TYPE", anchor="w")
+        self.user_inventory.heading("amount", text="SERVINGS", anchor="w")
+        self.user_inventory.heading("entry date", text="ENTRY DATE", anchor="w")
+        self.user_inventory.heading("notify (days)", text="NOTIFICATION DAY", anchor="w")
+        self.user_inventory.heading("expiration (days)", text="EXPIRATION DAY", anchor="w")
+
+        self.df_user_rows = self.df_user.to_numpy().tolist()
+        for row in self.df_user_rows:
+            self.user_inventory.insert("", "end", values=row)
+
+        #scrollbar
+        self.inv_scroll = Scrollbar(self.frame_edit, orient=VERTICAL, command=self.user_inventory.yview)
+        self.user_inventory.config(yscrollcommand=self.inv_scroll.set)
+        self.inv_scroll.place(relx=0.99, rely=0.54, relheight=0.87, anchor="e")
+
+        self.servings_drop = OptionMenu()
+
+        #serving dropdowns:
+        for serv in self.df_user["amount"]:
+            
+            pass
+
+    def delete_button(self):
+        self.pop_up_del = Toplevel(master)
+
+        self.del_label = Label(self.pop_up_del, text="Are you sure you want to delete this item?", font="roboto 12")
+        self.del_label.place(relx=0.5, rely=0.01, anchor="n")
+
+        self.del_button = Button(self.pop_up_del, text="DELETE", bd=0, command=self.delete_item)
+        self.del_button.place(relx=0.4, rely=0.1, anchor="n")
+
+        self.keep_button = Button(self.pop_up_del, text="KEEP", bd=0, command=self.dest_pop)
+        self.keep_button.place(relx=0.6, rely=0.1, anchor="n")
+
+
+    def delete_item(self):
+        pass
+
+    def dest_pop(self):
+        self.pop_up_del.destroy()
+
+    def change_amount(self):
+
 
 
 
