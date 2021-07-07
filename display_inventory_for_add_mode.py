aa9ee9910419e55dcfedf06bdcf81dc2b7c6f78b
Pinar <pinaradanir96@gmail.com>
2021-07-07 11:32:27 -0500

    changed the background etc for the items display

--- display_inventory_for_add_mode.py
+++ display_inventory_for_add_mode.py
@@ -1,5 +1,5 @@
 from os import name
-from tkinter.ttk import Treeview
+from tkinter.ttk import Style, Treeview
 import pandas as pd 
 import datetime
 from tkinter import * 
@@ -10,7 +10,17 @@ from class_food import Food
 from class_entry_food import Entry_Food
 
 
-
+#  style things:
+#main title = 22
+#other titles = 15
+#text = 11
+#bg="#FCF0E4"
+#dark pink ="#BE796D" (display box)
+#purple blue ="#A9B6BE" (main title ribbon)
+#light pink="#E9BFA7" (add box)
+#light green blue ="#C2D7D0" 
+#dark yellow ="#BA8E47" 
+#dark gray blue ="#576566" (edit box)
 
 
 
@@ -31,27 +41,26 @@ class Display_items(object):
         self.frame_top = Frame(master, bg="#A9B6BE")
         self.frame_top.place(relx=0.01, rely=0.01, relheight=0.06, relwidth=0.98)
         
-        #middle frame 
-        self.frame_middle = Frame(master, bg="#E9BFA7")
-        self.frame_middle.place(relx=0.01, rely=0.08, relheight=0.41, relwidth=0.98)
-        
+        #------------------------------------------------------------------------------------------------------
+
         #bottom frame
         self.frame_bottom = Frame(master, bg="#BE796D")
         self.frame_bottom.place(relx=0.01, rely=0.5, relheight=0.49, relwidth=0.98)
 
-        #------------------------------------------------------------------------------------
         #Display user item inventory:
         self.title_inventory = Label(self.frame_bottom, bg="#BE796D", text= "YOUR ITEMS", font="roboto 15")
         self.title_inventory.place(relx=0.5, anchor="n")
 
-        #showing the table w tksheet:
-        #self.user_inventory = Sheet(self.frame_bottom, data=self.df_user, height=300, width=700, frame_bg="#FCF0E4")#, show_header=True, show_y_scrollbar=True)
-        #self.user_inventory.place(relx=0.5, rely=0.1, anchor="n")
-        #self.user_inventory.enable_bindings(("single_select", "row_select", "column_width_resize", "arrowkeys", "right_click_popup_menu", "rc_select", "rc_insert_row", "rc_delete_row", "copy", "cut", "paste", "delete", "undo", "edit_cell"))
+        #treeview style:
 
-        #self.user_inventory.highlight_rows(rows=[0], bg="#C2D7D0", fg=None, highlight_index=True, redraw=False)
+        #self.today_items = self.df_user.query("entry date == @self.entry_date")
 
-        #w treeview:
+        self.style_tw = Style()
+        self.style_tw.theme_use("default")
+        self.style_tw.configure("Treeview", foreground="black", rowheight=25, fieldbackground="#FCF0E4")
+        self.style_tw.map("Treeview")
+
+        #treeview item display:
         self.user_inventory = Treeview(self.frame_bottom)
         self.user_inventory.place(rely=0.1, relx=0.5, relwidth=0.98, relheight=0.88, anchor="n")
 
@@ -78,12 +87,16 @@ class Display_items(object):
         for row in self.df_user_rows:
             self.user_inventory.insert("", "end", values=row)
 
+        #scrollbar
         self.inv_scroll = Scrollbar(self.frame_bottom, orient=VERTICAL, command=self.user_inventory.yview)
         self.user_inventory.config(yscrollcommand=self.inv_scroll.set)
         self.inv_scroll.place(relx=0.99, rely=0.54, relheight=0.87, anchor="e")
 
+        #highlight recent rows:
+        self.user_inventory.tag_configure("recent", background="#BA8E47")
+        self.user_inventory.tag_configure("others", background="#FCF0E4")
 
-        #for column in 
+
 
         
 e = Display_items(master)
