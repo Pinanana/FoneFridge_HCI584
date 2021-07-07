fe55eaaf1dc789afe72ada638ffeb7c133c277f2
Pinar <pinaradanir96@gmail.com>
2021-07-07 11:31:58 -0500

    graphic design things

--- add_mode.py
+++ add_mode.py
@@ -29,21 +29,6 @@ class Add(object):
         self.frame_middle = Frame(master, bg="#E9BFA7")
         self.frame_middle.place(relx=0.01, rely=0.08, relheight=0.49, relwidth=0.98)
 
-        #bottom frame
-        self.frame_bottom = Frame(master, bg="#BE796D")
-        self.frame_bottom.place(relx=0.01, rely=0.5, relheight=0.49, relwidth=0.98)
-
-
-        #main title-FONEFRIDGE
-        self.title = Label(self.frame_top, bg="#A9B6BE", text= "FONEFRIDGE", font="roboto 22")
-        self.title.pack(pady=14)
-
-        
-        # CALENDAR--------
-        self.calendar_button = Button(self.frame_top, image=self.date_image, bg="#A9B6BE", bd=0, command=self.calendar_entry)
-        self.calendar_button.grid(column=2, row=0, ipadx=5)
-        
-        
         #read .csv files here:
         self.df = pd.read_csv("popular_items_library.csv")
         self.df_user = pd.read_csv("user_items.csv")
@@ -64,14 +49,14 @@ class Add(object):
         self.type_entry.set("Please select type")
 
         self.food_type_dropdown = OptionMenu(self.frame_vars, self.type_entry, *self.food_type_list, command=self.generate_item_dropdown) 
-        self.food_type_dropdown.place(relx=0.05, rely=0.3)
+        self.food_type_dropdown.place(relx=0.2, rely=0.3, relwidth=0.27, anchor="n")
 
         #ITEM SELECT:
         self.entry_name = StringVar(self.frame_vars)
         self.entry_name.set("Please select type first")
         
         self.food_names_dropdown = OptionMenu(self.frame_vars, self.entry_name, "none") 
-        self.food_names_dropdown.place(relx=0.3, rely=0.3)
+        self.food_names_dropdown.place(relx=0.5, rely=0.3, relwidth=0.27, anchor="n")
 
         #SERVINGS SELECT:
         self.servings_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -80,7 +65,7 @@ class Add(object):
         self.servings_entry.set("Please select servings count")
 
         self.servings_dropdown = OptionMenu(self.frame_vars, self.servings_entry, *self.servings_list)
-        self.servings_dropdown.place(relx=0.65, rely=0.3)
+        self.servings_dropdown.place(relx=0.8, rely=0.3, relwidth=0.27, anchor="n")
 
         #selecting servings will trigger this:
         #if self.servings_entry != "Please select servings count" and self.entry_name != "Please select name" and 
@@ -99,22 +84,14 @@ class Add(object):
         self.result4.grid(row=3, column=0, padx=2, pady=2, sticky=W)
 
         #SAVE button:
-        self.save_button = Button(self.frame_middle, text="SAVE", command=self.save_item)
-        self.save_button.place(relx=0.35, rely=0.9)
+        self.save_button = Button(self.frame_middle, width=10, text="SAVE", command=self.save_item)
+        self.save_button.place(relx=0.4, rely=0.9, anchor="n")
 
         #DISCARD button:
-        self.discard_button = Button(self.frame_middle, text="DISCARD", command=self.erase_all)
-        self.discard_button.place(relx=0.55, rely=0.9)
+        self.discard_button = Button(self.frame_middle, width=10, text="DISCARD", command=self.erase_all)
+        self.discard_button.place(relx=0.6, rely=0.9, anchor="n")
 
         #------------------------------------------------------------------------------------
-        #Display user item inventory:
-        self.title_inventory = Label(self.frame_bottom, bg="#BE796D", text= "Your inventory:", font="roboto 15")
-        self.title_inventory.place(relx=0.03)
-
-        #showing the table w pandas:
-        
-
-
 
 
     def display_item(self):
@@ -142,33 +119,6 @@ class Add(object):
         self.type_entry.set("Please select type")
         self.entry_name.set("Please select type first")
         self.servings_entry.set("Please select servings count")
-
-
-
-    
-    # calendar pop-up window def here
-    def calendar_entry(self):  
-        self.pop_up = Toplevel(master)
-
-        self.date_label = Label(self.pop_up, text="Choose date", font="roboto 12")
-        self.date_label.pack(padx=10, pady=10)
-
-        self.entry_cal = DateEntry(self.pop_up, background= "#A9B6BE", foreground= "#576566")
-        self.entry_cal.pack(padx=10, pady=10)
-
-        self.ok_button = Button(self.pop_up, text="OK", bd=0, command=self.calendar_get)
-        self.ok_button.pack(padx=10, pady=10)
-    
-    def calendar_get(self):
-        self.entry_date = self.entry_cal.get_date()   #========================ENTRY DATE VARIABLE self.entry_date
-        print(self.entry_date)
-        self.notification_trigger()
-        self.pop_up.destroy()
-
-
-    #my items display:
-    #sort from the most recent.
-    
 
 #print(name)
 
