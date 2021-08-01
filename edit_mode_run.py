8360381f3cfadb4a7d00c98abfa66164d506c2b1
Pinar <pinaradanir96@gmail.com>
2021-08-01 11:13:10 -0500

    docstrings, while typing I realised a problem if there is no selection for amount, the change amount button was still working so I disabled it until a selection is made.

--- edit_mode_run.py
+++ edit_mode_run.py
@@ -16,40 +16,41 @@ canvas.pack()
 
 class Edit(object):
     """Class Edit is the EDIT mode of FoneFridge desktop app where the user can see their items and delete or change amount. 
-    The expired items have dark pink background, about tp expire items have medium pink background, okay to use items have cream background.
+    The expired items have dark pink background, about tp expire items have medium pink background, okay to use items have beige background.
+    Editing functions work with doubleclick and the editing tools dissapear anytime something is changed. 
     
     PLEASE make sure your Tk version is higher than 8.6.10.
     """
     def __init__(self, master):
+
+        #---------------------------------------------GENERAL-------------------------------------------------
+        #reading the .csv files here:
+        self.df = pd.read_csv("popular_items_library.csv")
+        self.df_user = pd.read_csv("user_items.csv")
+        #making the app frame:
         app_frame = Frame(master)
-        app_frame.pack() #geometry CHECK GUI LECTURE
+        app_frame.pack()
+        #---------------------------------------------GENERAL-------------------------------------------------
+
         #---------------------------------------------TOP RIBBON-------------------------------------------------
         #top title frame
         self.frame_top = Frame(master, bg="#A9B6BE")
         self.frame_top.place(relx=0.01, rely=0.01, relheight=0.056, relwidth=0.98)
 
-        # MAIN title-FONEFRIDGE------------
+        # MAIN TITLE:
         self.title = Label(self.frame_top, bg="#A9B6BE", text= "FONEFRIDGE", font="roboto 22")
         self.title.grid(column=1, row=0, ipadx=151)
 
-        # MODES--------
-        #button variable:
+        # MODE SWITCH:
         self.add_button = Button(self.frame_top, width=10, text="ADD MODE", font="roboto 15", bg="#A9B6BE",command=self.change_mode)
         self.add_button.grid(column=0, row=0, ipadx=5)
-        self.is_add = False
 
         #---------------------------------------------TOP RIBBON-------------------------------------------------
 
         #---------------------------------------------EDIT BODY-------------------------------------------------
-        #changing BOTTOM frame:
+        #DISPLAY FRAME:
         self.frame_edit = Frame(master, bg="#576566")
         self.frame_edit.place(relx=0.01, rely=0.08, relheight=0.81, relwidth=0.98)
-
-
-        #read .csv files here:
-        self.df = pd.read_csv("popular_items_library.csv")
-        self.df_user = pd.read_csv("user_items.csv")
-        
 
         # EDIT MODE DESIGN
 
@@ -61,22 +62,22 @@ class Edit(object):
         self.sub_title = Label(self.frame_edit, bg="#576566", text="Double click on an item to edit.", font="roboto 11")
         self.sub_title.place(relx=0.5, rely=0.05, anchor="n")
 
-        
+         #TREEVIEW:
+        #PLEASE MAKE SURE YOUR Tk VERSION IS HIGHER THAN 8.6.10!!
+
+        # treeview style:
         self.style_tw = Style()
         self.style_tw.theme_use("default")
         self.style_tw.configure("Treeview", foreground="black", rowheight=25, fieldbackground="#FCF0E4", background=("#FCF0E4"))
-        #self.style_tw.map("Treeview", foreground=self.fixed_map('foreground'), background=self.fixed_map('background'))
         self.style_tw.map("Treeview", foreground=[("selected", "#576566")], background=[("selected", "#C2D7D0")])
 
         #treeview item display:
         self.user_inventory = Treeview(self.frame_edit, selectmode=BROWSE)
         self.user_inventory.place(rely=0.1, relx=0.5, relwidth=0.98, relheight=0.88, anchor="n")
-
+        #getting the columns from user_items.csv file (self.df_user is the dataframe containing it)
         self.user_inventory["column"] = list(self.df_user.columns)
         self.user_inventory["show"] = "headings" # #0 column doesn't show when this line works.
-
-        #MAKE NEW COLUMN CALLED "-"
-        #THEN MAKE EMPTY FOR EVERYTHING BUT "X" FOR SELECTED
+        #creating the columns:
         self.user_inventory.column("#0", width=40)
         self.user_inventory.column("title", width=95)
         self.user_inventory.column("type", width=95)
@@ -84,7 +85,7 @@ class Edit(object):
         self.user_inventory.column("entry date", width=140)
         self.user_inventory.column("notify (days)", width=140)
         self.user_inventory.column("expiration (days)", width=140)
-
+        #creating the headings:
         self.user_inventory.heading("#0", text="-")
         self.user_inventory.heading("title", text="TITLE", anchor="w")
         self.user_inventory.heading("type", text="TYPE", anchor="w")
@@ -93,7 +94,7 @@ class Edit(object):
         self.user_inventory.heading("notify (days)", text="NOTIFICATION DAY", anchor="w")
         self.user_inventory.heading("expiration (days)", text="EXPIRATION DAY", anchor="w")
 
-        #for edit page where users won't see notifications, today is set to datetime.today
+        #for edit page today is set to datetime.today
         self.today = datetime.datetime.today().date().strftime("%Y-%m-%d")
 
         # tags:
@@ -101,6 +102,7 @@ class Edit(object):
         self.user_inventory.tag_configure("notified", background="#E9BFA7")
         self.user_inventory.tag_configure("others", background="#FCF0E4")
 
+        #inserting the values:
         #expired:
         self.df_expired = self.df_user.loc[self.df_user["expiration (days)"] <= self.today]
         self.df_expired_rows = self.df_expired.to_numpy().tolist()
@@ -126,34 +128,49 @@ class Edit(object):
 
         #edit & delete--------------------
         self.user_inventory.bind("<Double-1>", self.edit_tools)
-        #self.checked_img = PhotoImage(file="checked.png") #image=self.checked_img, 
-        #self.user_inventory.tag_configure("selected", background="#C2D7D0")
         
-
         #---------------------------------------------EDIT BODY-------------------------------------------------
 
-        #---------------------------------------------EDIT BOTTOMS-------------------------------------------------
+        #---------------------------------------------EDIT BUTTONS-------------------------------------------------
 
         #the bottom
         self.bottom_frame = Frame(master, bg="#576566")
         self.bottom_frame.place(relx=0.01, rely=0.89, relheight=0.1, relwidth=0.98)
-
+        #since the initial condition doesn't have any selection, the message requests a selection.
         self.changing_item_label = Label(self.bottom_frame, bg="#576566", text="Please double click on the item you want to edit.", font="roboto 15")
         self.changing_item_label.place(relx=0.5, rely=0.01, anchor="n")
 
-        #---------------------------------------------EDIT BOTTOMS-------------------------------------------------
+        #---------------------------------------------EDIT BUTTONS-------------------------------------------------
 
     #==============================DEFS=====================
     def change_mode(self):
+        """change_mode function closes one mode window and opens the other one.
+        In edit mode, it closes edit mode and runs add mode.
+        Args:
+            none
+        Returns:
+            the add mode window.
+        Raises:
+            none
+        """
         master.destroy()
         os.system("add_mode_run.py")
 
-    def highlight(self, e):
-        self.selected_item = self.user_inventory.selection()
-        if self.user_inventory.item(self.selected_item, "tags") != 'selected':
-            self.user_inventory.item(self.selected_item, tags=['selected'])
-
     def edit_tools(self, e):
+        """This function creates the delete button, servings selection dropdown menu, and change amount button.
+        Once the user double clicks on an item these widgets populate the bottom part with a title telling which item added when.
+
+        This function also catches the selection by .selection() function. 
+        Then the 0th item in the values list of that item is compared with names of the user_items.csv file (self.df_same_name).
+        Since there can be multiple items with the same name the entry date is also compared. 
+        Once the item is found, the index number is stored in self.index_select_number variable for later uses.
+        Args:
+            e: doubleclick on an item
+        Returns:
+            the editing tools: self.delete_but (button), self.serv_drop (dropdown menu), and self.serv_but (button)
+        Raises:
+            none
+        """
         #GETTING SELECTION
 
         self.selected_item = self.user_inventory.selection()
@@ -178,10 +195,34 @@ class Edit(object):
         self.serv_drop = Combobox(self.bottom_frame, value=self.servings_list, state="readonly")
         self.serv_drop.place(relx=0.5, rely=0.7, relwidth=0.2, anchor=CENTER)
 
-        self.serv_but = Button(self.bottom_frame, text="CHANGE AMOUNT", command=self.change_amount_button)
+        
+        self.serv_but = Button(self.bottom_frame, text="CHANGE AMOUNT", command=self.change_amount_button, state="disabled")
         self.serv_but.place(relx=0.9, rely=0.7, relwidth=0.28, anchor="e")
 
+        self.serv_drop.bind("<<ComboboxSelected>>", self.activate_button)
+
+    def activate_button(self, e):
+        """To prevent user error if they click on change amount without selecting amount, the button is originally disabled. 
+        This function only changes the state of the button from disabled to normal.
+        Args:
+            e: selection in derv_drop dropdown menu
+        Returns:
+            self.serv_but button is usable now
+        Raises:
+            none
+        """
+        self.serv_but.config(state="normal")
+
+
     def delete_button(self):
+        """once the delete button is clicked, a user error preventing pop up appears. 
+        Args:
+            clicking self.delete_but
+        Returns:
+            self.pop_up_del that asks the user if they are sure to delete.
+        Raises:
+            none
+        """
         self.pop_up_del = Toplevel(master)
         self.pop_up_del.geometry("500x50")
 
@@ -195,6 +236,15 @@ class Edit(object):
         self.keep_button.place(relx=0.6, rely=0.5, anchor="n")
 
     def change_amount_button(self):
+        """once the change amount button is clicked, a user error preventing pop up appears.
+        In the message the original amount and new amount are stated. 
+        Args:
+            clicking self.serv_but
+        Returns:
+            self.pop_up_amount that asks if they want to change amount from self.select_amo to self.drop.get()
+        Raises:
+            none
+        """
         self.pop_up_amount = Toplevel(master)
         self.pop_up_amount.geometry("500x50")
 
@@ -211,6 +261,19 @@ class Edit(object):
         
 
     def delete_item(self):
+        """If the user clicks delete in the self.pop_up_del window, the item is dropped from the user_items.csv file. 
+        From the edit_tool function, the selection index number was stored in self.index_select_num.
+        That number is used to drop that exact item from the user_items.csv file.
+        After deleting the item, the treeview is updated, the title goes back to no selection condition, buttons and dropdown menu are removed, and finally the pop up window gets closed. 
+        Args:
+            clicking self.del_button
+        Returns:
+            updated .csv file
+            editing tools are removed
+            self.update_treeview()
+        Raises:
+            none
+        """
         self.df_user.drop(self.index_select_number, inplace=True)
         self.df_user.to_csv("user_items.csv", index=False)
         self.update_treeview()
